a30e503139322c46fe2c5809bbb16fd68a805f05
descoles <descoles@iu.edu>
2023-07-06 20:09:11 -0400

    id rename done

--- nanobody_database_analysis/merge_script.py
+++ nanobody_database_analysis/merge_script.py
@@ -10,3 +10,23 @@ sequence = sequence[ ['id'] + [col for col in sequence.columns if col != 'id'] ]
 sequence['file type'] = 'patent sequence'
 metaData['file type'] = 'patent metaData'
 # giving the CSV files a new column for easier organization after merging
+
+sequence_dups = sequence['id'].duplicated()
+print(sequence.head())
+print(sequence[sequence_dups])
+
+suffixes = {}
+for i,row in sequence.iterrows():
+    id_value = row['id'] # id value = random num in 'id' column
+    if sequence_dups[i]: # i = line from sequence file so here we check if that line is in the duplicated series
+        if id_value in suffixes:
+            suffixes[id_value] += 1
+            sequence.at[i,'id'] = f"{id_value}-{suffixes[id_value]}"
+        
+        else:
+            suffixes[id_value] = 1
+            sequence.at[i,'id'] = f"{id_value}-{suffixes[id_value]}"
+
+
+sequence.to_csv('modified_sequence.csv', index=False)
+
